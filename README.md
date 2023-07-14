e710a4f7ce60d9dd1bb195b03befbb7e5576a8dc
itymarcel <marcel@hypersuper.uk>
2023-07-14 17:49:02 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# notion-table-as-cms
-Simple node.js repo to fetch a Notion table including each pages attributes and content, turn it into HTML and return it to the client.
+# Use a Notion Table Database as a super simple CMS
+Simple node.js repo to fetch a Notion table via NotionAPI, including each page's attributes and its content. It translates the content into HTML and adds it to the objects, then returns the entire thing as a JSON to the client.
+
+## On the client side, you only have to do this.
+```javascript
+const server: string = 'your-server-address';
+
+fetch(server)
+  .then((response) => response.json())
+  .then((response) => {
+      console.log('response ', response);
+  })
+```
---
+++ server.js
@@ -0,0 +1,99 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { Client } from "@notionhq/client";
+
+const server = express();
+const port = 3001;
+const notion = new Client({ auth: process.env.NOTION_KEY });
+const databaseId = process.env.NOTION_DATABASE_ID;
+
+dotenv.config();
+server.use(cors({
+    origin: '*'
+}));
+
+server.get('/', async (req, res) => {
+    try {
+        const response = await queryDatabase(databaseId);
+        const pagePromises = response.results.map(async (page) => {
+            const pageContent = await retrievePage(page.id);
+            return {
+                ...page,
+                pageContent: jsonToHtml(pageContent)
+            };
+        });
+
+        const pagesWithContent = await Promise.all(pagePromises);
+        res.json(pagesWithContent);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    }
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+async function queryDatabase(databaseId) {
+    try {
+        const response = await notion.databases.query({
+            database_id: databaseId
+        });
+        return response;
+    } catch (error){
+        console.log(error.body);
+    }
+}
+
+async function retrievePage(pageId) {
+    try {
+        const response = await notion.blocks.children.list({
+            block_id: pageId,
+            page_size: 100,
+          });
+        return response;
+    } catch (error){
+        console.log(error.body);
+    }
+}
+
+const jsonToHtml = (json) => {
+    let html = '';
+    console.log(json);
+    json.results.forEach(block => {
+        console.log(block);
+        switch (block.type) {
+            case 'heading_1':
+                html += `<h1>${block.heading_1.rich_text[0].plain_text}</h1>`;
+                break;
+            case 'heading_2':
+                html += `<h2>${block.heading_2.rich_text[0].plain_text}</h2>`;
+                break;
+            case 'heading_3':
+                html += `<h3>${block.heading_3.rich_text[0].plain_text}</h3>`;
+                break;
+            case 'paragraph':
+                let paragraph = '';
+                block.paragraph.rich_text.forEach(text => {
+                    if (text.href) {
+                        paragraph += `<a href="${text.href}">${text.plain_text}</a>`;
+                    } else {
+                        paragraph += text.plain_text;
+                    }
+                });
+                html += `<p>${paragraph}</p>`;
+                break;
+            case 'quote':
+                html += `<blockquote>${block.quote.rich_text[0].plain_text}</blockquote>`;
+                break;
+            case 'image':
+                html += `<img src="${block.image.file.url}" alt="${block.image.caption[0]?.plain_text || ''}"/>`;
+                break;
+            default:
+                console.log(`Unsupported block type: ${block.type}`);
+        }
+    });
+    return html;
+}
